6e43ad17ab5a217d1cb888334ae1bbe473e075ed
Mysan <mysanr@emea.roche.com>
2018-01-24 10:01:04 +0100

    added test on log in failed

--- src/main/test/groovy/scrumweb/security/SecurityTest.groovy
+++ src/main/test/groovy/scrumweb/security/SecurityTest.groovy
@@ -1,9 +1,8 @@
 package scrumweb.security
 
 import common.TestData
-import org.mockito.Mock
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
@@ -16,9 +15,12 @@ import scrumweb.user.account.repository.UserAccountRepository
 import scrumweb.user.account.service.UserAccountService
 import spock.lang.Specification
 
+import javax.naming.AuthenticationException
+
 import static infrastructure.JsonResultMatcher.jsonResponse
 import static infrastructure.MockHttpHelper.httpHelper
 import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.when
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 class SecurityTest extends Specification{
@@ -26,23 +28,26 @@ class SecurityTest extends Specification{
     SecurityContextService securityContextService
     MockMvc mockMvc
 
-    private final static JSON_CONTENT = "{username: 'testUser', password: 'testUser'}"
+    Authentication authentication = Mock(Authentication.class)
+    SecurityContext securityContext = Mock(SecurityContext.class)
+    UserDetails userDetails = Mock(UserDetails.class)
+    JwtTokenUtil jwtTokenUtil = mock(JwtTokenUtil.class)
+    UserAccountService userAccountService = mock(UserAccountService.class)
+    JwtUserDetailsServiceImpl jwtUserDetailsService = mock(JwtUserDetailsServiceImpl.class)
+    UserAccountRepository userAccountRepository = Mock(UserAccountRepository.class)
+    AuthenticationManager authenticationManager = mock(AuthenticationManager.class)
+
+    private final static String USERNAME = "testUser"
+    private final static String PASSWORD = "testUser"
+    private final static String JSON_CONTENT = "{username: " + USERNAME + ", password: " + PASSWORD + "}"
 
     def setup() {
-        Authentication authentication = Mock(Authentication.class)
-        SecurityContext securityContext = Mock(SecurityContext.class)
-        UserDetails userDetails = Mock(UserDetails.class)
-        JwtTokenUtil jwtTokenUtil = mock(JwtTokenUtil.class)
-        UserAccountService userAccountService = mock(UserAccountService.class)
-        JwtUserDetailsServiceImpl jwtUserDetailsService = mock(JwtUserDetailsServiceImpl.class)
-        UserAccountRepository userAccountRepository = Mock(UserAccountRepository.class)
-        AuthenticationManager authenticationManager = Mock(AuthenticationManager.class)
 
         securityContext.getAuthentication() >> authentication
-        authentication.getName() >> "testUser"
-        jwtUserDetailsService.loadUserByUsername("testUser") >> userDetails
-        userAccountRepository.findByUsername("testUser") >> TestData.USER_ACCOUNT
-        userDetails.getUsername() >> "testUser"
+        authentication.getName() >> USERNAME
+        jwtUserDetailsService.loadUserByUsername(USERNAME) >> userDetails
+        userAccountRepository.findByUsername(USERNAME) >> TestData.USER_ACCOUNT
+        userDetails.getUsername() >> USERNAME
 
         SecurityContextHolder.setContext(securityContext)
         securityContextService = new SecurityContextService(userAccountRepository)
@@ -54,15 +59,25 @@ class SecurityTest extends Specification{
     def "should return logged user account"() {
         expect:
         securityContextService.getCurrentUserAccount() != null
-        securityContextService.getCurrentUserAccount().getUsername() == "testUser"
+        securityContextService.getCurrentUserAccount().getUsername() == USERNAME
     }
 
-    def "should perform login request"() {
+    def "should perform log in request"() {
         when:
         def request = mockMvc.perform(httpHelper().requestPost("/auth", JSON_CONTENT))
 
         then:
         request.andExpect(status().isOk())
                 .andExpect(jsonResponse().notEmpty())
+    }
+
+    def "should fail on log in request"() {
+        when(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(USERNAME, PASSWORD))).thenThrow(AuthenticationException)
+
+        when:
+        mockMvc.perform(httpHelper().requestPost("/auth", JSON_CONTENT))
+
+        then:
+        thrown Exception
     }
 }
