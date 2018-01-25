ed9aaac8b19e9e784eea9066cae3416df6de7ab6
Patryk Bados <patryk.bados@gmail.com>
2018-01-25 08:48:07 +0100

    edtied tests in project service

--- src/main/test/groovy/scrumweb/project/service/ProjectServiceTest.groovy
+++ src/main/test/groovy/scrumweb/project/service/ProjectServiceTest.groovy
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
 import scrumweb.App
+import scrumweb.dto.ProjectDto
+import scrumweb.exception.ProjectNotFoundException
+import scrumweb.user.project.domain.Project
 import scrumweb.user.project.repository.ProjectRepository
 import scrumweb.user.project.service.ProjectService
 import spock.lang.Specification
@@ -27,5 +30,29 @@ class ProjectServiceTest extends Specification{
         then:
         projectRepository.findByName(TestData.PROJECT_DTO.getName()) != null
 
+    }
+
+    def "should edit project name"() {
+        projectRepository.getOne(1L) >> new Project()
+        given:
+        ProjectDto PROJECTDTO = new ProjectDto("projectname", "description", TestData.USER_PROFILE, "icon", TestData.USER_PROFILE_SET)
+
+        when:
+        ProjectDto editedProject = projectService.editName("edited", 1L)
+
+        then:
+        editedProject.getName() == PROJECTDTO.getName()
+    }
+
+    def "should throw exception project not found"() {
+        projectRepository.getOne(2L) >> null
+        ProjectDto PROJECTDTO = new ProjectDto("projectname", "description", TestData.USER_PROFILE, "icon", TestData.USER_PROFILE_SET)
+
+        when:
+        projectService.editName("edited", 2L)
+
+        then:
+        ProjectNotFoundException ex = thrown()
+        ex.message == "Project with id 2 not found!"
     }
 }
