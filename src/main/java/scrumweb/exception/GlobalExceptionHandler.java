c8c1c7bc46a41f08a6a76850c605489ccfdac9bb
Patryk <patryk.bados@gmail.com>
2018-01-29 11:08:05 +0100

    added global exception handler (#9)

---
+++ src/main/java/scrumweb/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package scrumweb.exception;
+
+import javassist.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFoundException(Exception ex){
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String handleBadCredentailsException(){
+        return "Sorry, Username or password is incorrect!";
+    }
+
+    @ExceptionHandler(ProjectNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleProjectNotFoundException(Exception ex){
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(ProjectAlreadyExsistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleProjectAlreadyExistsException(Exception ex){
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleUserAlreadyExistsException(Exception ex){
+        return ex.getMessage();
+    }
+}
