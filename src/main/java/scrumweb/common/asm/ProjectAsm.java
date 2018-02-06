abeef1847cc5b8823a167ec95e1f85ac478ea791
Aviosv6 <krzsiarkowski@gmail.com>
2018-02-06 09:03:10 +0100

    changed from searching by name to by key on backend (#17)
    Added project key at backend

--- src/main/java/scrumweb/common/asm/ProjectAsm.java
+++ src/main/java/scrumweb/common/asm/ProjectAsm.java
@@ -8,21 +8,19 @@ import scrumweb.project.domain.Project;
 import scrumweb.project.domain.ProjectMember.Role;
 import scrumweb.project.domain.ProjectMember;
 
-import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Component
 public class ProjectAsm {
     public Project makeProject(ProjectDto projectDto) {
-        return new Project(projectDto.getName(), projectDto.getDescription(), projectDto.getIcon());
+        return new Project(projectDto.getName(), projectDto.getDescription(), projectDto.getIcon(), projectDto.getProjectKey());
     }
 
     public ProjectDto makeProjectDto(Project project) {
         return new ProjectDto(project.getName(), project.getDescription(), project.getIcon(),
                 project.getMembers().stream()
                         .map(member -> makeProjectMemberDto(member, project.getId()))
-                        .collect(Collectors.toSet()));
+                        .collect(Collectors.toSet()), project.getKey());
     }
 
     public ProjectMember makeProjectMember(UserAccount userAccount, Role role){
