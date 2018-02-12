bf3c30d0c87f848d256aab6067bf8e76c31450cf
Patryk <patryk.bados@gmail.com>
2018-02-12 11:13:41 +0100

    Create footer (#20)
    * created footer component
    
    * edited css
    
    * created footer and fixed problem with justify-content-center

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProjectComponent } from './project/project.component';
 import {ProjectService} from "./project/project.service";
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import {ProjectService} from "./project/project.service";
     RegistrationComponent,
     HomeComponent,
     NavbarComponent,
-    ProjectComponent
+    ProjectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
