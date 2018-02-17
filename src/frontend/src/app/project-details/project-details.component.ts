3f6e3a017212d455fcd26659afe8f484b0ef8e8a
Mysan <mysanr@emea.roche.com>
2018-02-17 23:03:10 +0100

    changes in styles

--- src/frontend/src/app/project-details/project-details.component.ts
+++ src/frontend/src/app/project-details/project-details.component.ts
@@ -26,6 +26,7 @@ export class ProjectDetailsComponent implements OnInit {
   ngOnInit() {}
 
   public selectIssue(issueId: number) {
+    console.log('HAYA');
     this.selectedIssue = this.projectDetails.issues.find(issue => issue.id === issueId);
   }
 
