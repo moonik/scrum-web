bb875b023d76ba136cc729552d44c800a3d1be8f
Mysan <mysanr@emea.roche.com>
2018-05-08 14:56:07 +0200

    fixed sonar issues

--- src/main/java/scrumweb/notification/controller/NotificationController.java
+++ src/main/java/scrumweb/notification/controller/NotificationController.java
@@ -2,13 +2,9 @@ package scrumweb.notification.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import scrumweb.dto.notification.NotificationDto;
 import scrumweb.notification.service.NotificationService;
-
-import java.util.List;
 
 import static scrumweb.common.ApplicationConstants.API_URL;
 
--- src/main/java/scrumweb/notification/service/NotificationService.java
+++ src/main/java/scrumweb/notification/service/NotificationService.java
@@ -3,15 +3,11 @@ package scrumweb.notification.service;
 import lombok.AllArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-import scrumweb.common.SecurityContextService;
 import scrumweb.dto.notification.NotificationDto;
 import scrumweb.notification.domain.Notification;
 import scrumweb.notification.repository.NotificationRepository;
 import scrumweb.user.account.domain.UserAccount;
 import scrumweb.user.account.repository.UserAccountRepository;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
